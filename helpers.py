adfe86f0c3dac1af82dac2ecf1ce98e529591be7
ChanhBuiNorthwestern <chanh.bui@northwestern.edu>
2022-11-07 21:08:05 -0600

    Add hugging face API

--- helpers.py
+++ helpers.py
@@ -1,6 +1,10 @@
 import requests
 import praw
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+import json
+import requests
+
 '''
 For testing purposes and helper functions
 '''
@@ -20,3 +24,20 @@ def search_reddit(s):
     return comments
 print(search_reddit('Life is hard?'))
 
+API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+api_token = "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"
+headers = {"Authorization": f"Bearer {api_token}"}
+
+def similarity (source_sentence, other_sentences):
+    query = {
+        "inputs": {
+            "source_sentence": source_sentence,
+            "sentences":other_sentences
+        }
+    }
+    response = requests.post(API_URL, headers=headers, json = query)
+    return response.json()
+
+data = similarity("I'm very happy", ["I'm filled with happiness", "I'm happy"])
+
+print(data)
